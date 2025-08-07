349c6a23bae1583457bed8cb43290e69214f28ad
Himalay <your-noreply-email@users.noreply.github.com>
2025-08-07 05:42:04 +0530

    Fixed Api Error's, Himalay Final_V1

--- src/main/java/com/example/DuplicateFinder/controller/ScanController.java
+++ src/main/java/com/example/DuplicateFinder/controller/ScanController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
